ddc7776a7d14d207b434fac5f2250809d2fbf702
Gor Julhakyan <gorjulhakyan90@gmail.com>
2016-01-15 17:55:13 +0400

    UD AMI

--- taxi/client/lib/app.js
+++ taxi/client/lib/app.js
@@ -6,14 +6,14 @@ angular.module('myPersonal', [
 //	'uiGmapgoogle-maps', 
 	'ngMaterial'
 ])
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('green')
-    .accentPalette('orange');
-    var blueTheme = $mdThemingProvider.theme('blueTheme', 'default');
-  var bluePalette = $mdThemingProvider.extendPalette('blue', {
-    '500': '#03A9F4'
-  });
-  $mdThemingProvider.definePalette('bluePalette', bluePalette);
-  blueTheme.primaryPalette('bluePalette');
+
+.config(($mdIconProvider) => {
+  $mdIconProvider
+    .iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg")
+    .iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg")
+    .iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg")
+    .iconSet("content", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg")
+    .iconSet("toggle", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg")
+    .iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg")
+    .iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg");
 });
