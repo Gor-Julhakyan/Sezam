3f4a063d2d7c0c701c89f2101710b31561ece4ea
Gor-Julhakyan <gorjulhakyan90@gmail.com>
2016-01-23 19:57:32 +0400

    -..

--- taxi/client/parties/parties-list/parties-list.component.js
+++ taxi/client/parties/parties-list/parties-list.component.js
@@ -8,34 +8,37 @@ angular.module('myPersonal').directive('partiesList', function () {
 
       this.newParty = [];
 
+      this.db = function () {
+        var ob = this.newParty;
+        var lent = ob.length;
+        var totalArray = [];
+        var total = 0;
+        console.log(lent);
 
-this.aray = {};
+        if (lent > 0) {
+          for (var i = 0; i < lent; i++) {
+            parseInt(i, 10);
 
-this.db = function () {
-  var ob = this.newParty;
-  var lent = ob.length;
-  console.log(lent);
+            if (ob[i].sum1 && ob[i].sum2 && ob[i].sum3 > 0) {
+              var sum = ob[i].sum1 + ob[i].sum2 + ob[i].sum3;
+            } else if (ob[i].sum1 && ob[i].sum2 > 0) {
+              var sum = ob[i].sum1 + ob[i].sum2;
+            } else {
+              var sum = ob[i].sum1;
+            }
 
-  if (lent > 0) {
-    for (var i = 0; i < lent; i++) {
-      parseInt(i, 10);
-      if (ob[i].sum1 === null) {
-        console.log(ob[i].sum1 + "is Null");
-      }
-      var sum = ob[i].sum1 + ob[i].sum1 + ob[i].sum3;
-      this.newParty[i].total = sum;
-/*
-      this.newParty = this.newParty.concat([
-        {total : sum},
-      ]);
-      */
-      console.log('I');
-      console.log(sum);
-      //console.log(i);
-    } 
-  }
+            this.newParty[i].total = sum;
 
-}
+            totalArray[i] = sum;
+            total =1000;
+            
+            this.newParty.total = total;
+            console.log(total);
+          } 
+          
+        }
+
+        }
 
       this.helpers({
         parties: () => {
@@ -46,12 +49,6 @@ this.db = function () {
         }
       });
 
- console.log('Ther IS Mongo');     
- console.log(this.newParty);
- console.dir(this.newParty);
- console.log('Mongo End');
-
-
       this.addParty = () => {
         Parties.insert(this.newParty);
         this.newParty = {};
@@ -61,51 +58,6 @@ this.db = function () {
         Parties.remove({_id: party._id});
       }
 
-      this.carsErevan = [
-
-      ];
-this.sum1 = 0;
-this.a = 0;
-this.calc = 0;
-this.tot = 0;
-
-this.abc = function (a1, a2, a3) {
-  this.s1=a1
-this.s2=a2
-this.s3=a3
-  this.tot = this.s1 + this.s2 + this.s3;
-  //console.log(this.tot);
-  return this.tot;
-}
-
-      this.cmpny = "erevan";
-    //  this.partiesList.newParty[car.dbKey].sum1 =;
-    //  console.log(this.partiesList.newParty[car.dbKey].sum1);
- this.vobj = []; 
-  this.total = () => {
-        var sum1 = document.getElementById('sum1').value;
-        var sum2 = document.getElementById('sum2').value;
-        var sum3 = document.getElementById('sum3').value;
-        var s1 = parseInt(sum1 ,10);
-        var s2 = parseInt(sum2 ,10);        
-        var s3 = parseInt(sum3 ,10);
-        var sum = s1 + s2 + s3;
-       // console.log(sum);
-        document.getElementById('ttt').value=sum;
-
-        /*
-        this.s1 = parseInt(sum1, 10);
-        this.s2 = parseInt(sum2, 10);
-        this.s3 = parseInt(sum3, 10);
-    //    this.vobj.push(parseInt(sum1));
-     //   console.log(this.vobj);
-        this.calc = this.s1 + this.s2 + this.s3;
-       //console.log(total);
-        return this.calc;*/
-      };
-
-
-     // this.aaaa = this.total()
     } 
   }
 });
